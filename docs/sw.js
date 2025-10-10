2ee18b631e4cd81eea5fcee1f48a5d8b33a5b514
Jonathan Maietta <jonathanmaietta@gmail.com>
2025-10-10 14:38:22 -0400

    Add files via upload

---
+++ docs/sw.js
@@ -0,0 +1,31 @@
+const CACHE = 't2d-pulse-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192x192.png',
+  './icons/icon-256x256.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png',
+  './icons/icon-180x180.png',
+  './favicon-16x16.png',
+  './favicon-32x32.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin === self.location.origin) {
+    e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
+  }
+});
